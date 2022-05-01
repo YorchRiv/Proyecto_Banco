6f29ca0b306ee9bc3f7976a50698b972471386e7
Jorge Mejicanos <jorgemejicanos145@gmail.com>
2022-05-01 11:09:50 -0600

    Inicial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def run():
+    print("Bienvenido al Banco de Guatemala")
+    print("Seleecione una Opcion:")
+    print("1.) Manejo de Cuentas Personales:")
+    print("2.) Cuentas de Terceros:")
+    print("3.) Ver Cuenta Actual:")
+    print("4.) Realizar Deposito:")
+    print("5.) Realizar Retiro:")
+
+if __name__ == '__main__':
+    run()
